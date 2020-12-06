266d2fcc13dd3a1476b8ff53c5911eac5b5ca99d
Halalai Mircea <boss.mircea67@yahoo.com>
2020-12-06 22:08:21 +0200

    Add first screen of meme editor

---
+++ app/src/main/java/ro/unibuc/cs/memeow/ui/TemplateRecyclerViewAdapter.kt
@@ -0,0 +1,37 @@
+package ro.unibuc.cs.memeow.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ro.unibuc.cs.memeow.databinding.LayoutTemplateItemBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [DummyItem].
+ */
+class TemplateRecyclerViewAdapter :
+    RecyclerView.Adapter<TemplateRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutTemplateItemBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //val item = values[position]
+        holder.binding.templateTitle.text = "Sample memez!"
+
+    }
+
+    override fun getItemCount(): Int = 32
+
+    inner class ViewHolder(internal val binding: LayoutTemplateItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        override fun toString(): String {
+            return super.toString() + " '" + binding.templateTitle + "'"
+        }
+    }
+}
